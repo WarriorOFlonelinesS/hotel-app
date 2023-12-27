f689a149d5ff3557bce4fb6c232be3bf3f60cca5
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2023-12-27 09:33:56 +0200

    the first task

--- app/page.tsx
+++ app/page.tsx
@@ -11,25 +11,7 @@ export default function Home() {
   const { rooms } = useAppSelector(state => state.roomsReducer)
   return (
     <main >
-      <div>
-        {accounts.map((item: { password: string, image: string }) => {
-          return (
-            <div>
-              <p>{item.password}</p>
-              <Image width={100} height={100} src={item.image} alt="avatar"></Image>
-            </div>)
- 
-        })
-        }
-      </div>
-      {/* <div>
-        {rooms.gallery.map(image =>{return(<Image width={100} height={100} src={image} alt="avatar"></Image>)})}
-      </div> */}
-      {
-        console.log(rooms)
-      }
-      <button onClick={() => { dispatch(getAccounts()) }}>GET ACCOUNTS</button>
-      <button onClick={() => { dispatch(getRooms()) }}>GET ROOMS</button>
+      
     </main>
   )
 }
